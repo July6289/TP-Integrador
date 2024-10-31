a053ca8ff998f1fd3fe8705a18bf72b7cb445107
Lucas Hourcade <lucasivanhourcade@gmail.com>
2024-10-31 11:10:08 -0300

    navegacion entre rutas
    agregamos todos los botones que te permiten interactuar con las paginas

--- pokebox/src/app/componentesPokemon/lista-equipo-pokemon/lista-equipo-pokemon.component.ts
+++ pokebox/src/app/componentesPokemon/lista-equipo-pokemon/lista-equipo-pokemon.component.ts
@@ -1,12 +1,21 @@
+import { Router, RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-equipo-pokemon',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './lista-equipo-pokemon.component.html',
   styleUrl: './lista-equipo-pokemon.component.css'
 })
 export class ListaEquipoPokemonComponent {
+
+  constructor(private router: Router)
+  {}
+
+  goToCrearEquipo()
+  {
+    this.router.navigate(['/equipo-pokemon']);
+  }
 
 }
--- pokebox/src/app/componentesPokemon/pagina-principal/pagina-principal.component.ts
+++ pokebox/src/app/componentesPokemon/pagina-principal/pagina-principal.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { PerfilComponent } from '../perfil/perfil.component';
+import { ListaEquipoPokemonComponent } from "../lista-equipo-pokemon/lista-equipo-pokemon.component";
+import { ListaPokemonComponent } from "../lista-pokemon/lista-pokemon.component";
+import { CajaComponent } from "../caja/caja.component";
+import { InfoPokedexComponent } from "../info-pokedex/info-pokedex.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pagina-principal',
   standalone: true,
-  imports: [PerfilComponent],
+  imports: [PerfilComponent, ListaEquipoPokemonComponent, ListaPokemonComponent, CajaComponent, InfoPokedexComponent],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
 export class PaginaPrincipalComponent {
+  constructor(private router: Router)
+  {}
 
+  goToPerfil()
+  {
+    this.router.navigate(['/perfil']);
+  }
 }
