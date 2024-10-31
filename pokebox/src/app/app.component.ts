346e9e66dc99d2966a48cfd6845c0befc4384988
Lucas Hourcade <lucasivanhourcade@gmail.com>
2024-10-31 12:54:42 -0300

    seguimos con las restructuraciones
    Ahora el fondo cambia si accedes a otra pagina

--- pokebox/src/app/app.component.ts
+++ pokebox/src/app/app.component.ts
@@ -2,13 +2,15 @@
  import { RouterOutlet } from '@angular/router';
  import { CommonModule } from '@angular/common';
  import { HttpClientModule } from '@angular/common/http';
+ import { PaginaPrincipalComponent } from './componentesPokemon/pagina-principal/pagina-principal.component';
+
 //  import ColorThief from 'colorthief';
 
 
  @Component({
    selector: 'app-root',
  standalone: true,
-   imports: [RouterOutlet, CommonModule,HttpClientModule],
+   imports: [PaginaPrincipalComponent,RouterOutlet, CommonModule,HttpClientModule],
   templateUrl: './app.component.html',
  styleUrls: ['./app.component.css']
  })
--- pokebox/src/app/componentesPokemon/pagina-principal/pagina-principal.component.ts
+++ pokebox/src/app/componentesPokemon/pagina-principal/pagina-principal.component.ts
@@ -5,17 +5,21 @@ import { ListaPokemonComponent } from "../lista-pokemon/lista-pokemon.component"
 import { CajaComponent } from "../caja/caja.component";
 import { InfoPokedexComponent } from "../info-pokedex/info-pokedex.component";
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'pagina-principal',
   standalone: true,
-  imports: [PerfilComponent, ListaEquipoPokemonComponent, ListaPokemonComponent, CajaComponent, InfoPokedexComponent],
+  imports: [PerfilComponent, ListaEquipoPokemonComponent, ListaPokemonComponent, CajaComponent, InfoPokedexComponent, CommonModule],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
 export class PaginaPrincipalComponent {
   constructor(private router: Router)
   {}
+  color='brown';
+
+
 
   goToPerfil()
   {
