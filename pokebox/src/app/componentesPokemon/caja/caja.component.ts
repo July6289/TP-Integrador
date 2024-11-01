137d0d34a478c152ee043c19628dbd40b551b426
Nicolas Nieto <101644294+N1colasnieto@users.noreply.github.com>
2024-11-01 12:36:48 -0300

    caja cenrtal en proceso
    comenzamos con la estructuracion de la parte central de la página principal y luego solucionamos algunos eroores de otros componentes aledaños.

--- pokebox/src/app/componentesPokemon/caja/caja.component.ts
+++ pokebox/src/app/componentesPokemon/caja/caja.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-caja',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './caja.component.html',
   styleUrl: './caja.component.css'
 })
 export class CajaComponent {
-
 }
