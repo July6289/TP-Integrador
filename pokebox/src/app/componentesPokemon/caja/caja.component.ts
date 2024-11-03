81eb3048a7b8bae58baae293aed9f718a2826db0
malotes-en-el-techo <lucasivanhourcade@gmail.com>
2024-11-03 13:05:23 -0300

    agregar pokemon a la caja
    arreglando que se agregan mas de 30

--- pokebox/src/app/componentesPokemon/caja/caja.component.ts
+++ pokebox/src/app/componentesPokemon/caja/caja.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../interfazpokemon/interfazpokemon.inteface';
 
 @Component({
   selector: 'app-caja',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './caja.component.html',
-  styleUrl: './caja.component.css'
+  styleUrls: ['./caja.component.css']
 })
 export class CajaComponent {
+  pokemonesEnCaja: Pokemon[] = []; // Lista de Pokémon en la caja
+
+  agregarPokemon(pokemon: Pokemon) {
+    this.pokemonesEnCaja.push(pokemon);
+  }
 }
--- pokebox/src/app/componentesPokemon/lista-pokemon/lista-pokemon.component.ts
+++ pokebox/src/app/componentesPokemon/lista-pokemon/lista-pokemon.component.ts
@@ -1,80 +1,60 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Pokemon } from '../../interfazpokemon/interfazpokemon.inteface';
 import { PokeservicesService } from '../../pokeservices/pokeservices.service';
-import { Generation } from '../../interfazpokemon/interfazGeneracion.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-pokemon',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-pokemon.component.html',
-  styleUrl: './lista-pokemon.component.css'
+  styleUrls: ['./lista-pokemon.component.css']
 })
 export class ListaPokemonComponent {
+  listaPokemon: Pokemon[] = [];
+  pokemonSeleccionado: number | null = null;  // Índice del Pokémon seleccionado
+  @Output() pokemonAgregado = new EventEmitter<Pokemon>(); // Evento para enviar el Pokémon a la caja
 
+  pokeService = inject(PokeservicesService);
 
-listaPokemon?:Pokemon[]=[];
+  seleccionarPokemon(indice: number) {
+    this.pokemonSeleccionado = indice;
+  }
 
-
-pokeService=inject(PokeservicesService);
-
-numeroGeneracion:number|undefined;
-
-buscarPokemonPorGeneracion(dato:any)
-{
-  if(dato.target.value!==''){
-      console.log(dato.target.value);
-      this.pokeService.getPokemonByGeneration(dato.target.value).subscribe(
-        {
-          next: (respuesta:Generation|undefined)=>{
-
-          if(respuesta!=undefined)
-          {
-
-          respuesta.pokemon_species.map((pokemonEspecie)=>{
-
-            this.pokeService.getPokemonByName(pokemonEspecie.name).subscribe(
-
-              {
-                next: (poke: Pokemon | undefined) => {
-                  if(poke!=undefined)
-                  {
-                    this.listaPokemon?.push(poke);
-                  }
-
-                },
-                error: (err) => {
-                  console.log(err);
-
-                }
-              }
-            )
-
-
-          })
-
-        }
-
-
-
-          },
-          error: (e: Error) => {
-            console.log(e.message)
-          }
-        }
-
-      )
-
-
-    }
-    else{
-      this.listaPokemon=[];
-
+  agregarPokemonACaja() {
+    if (this.pokemonSeleccionado !== null) {
+      const pokemon = this.listaPokemon[this.pokemonSeleccionado];
+      this.pokemonAgregado.emit(pokemon);  // Emite el Pokémon seleccionado
+      this.listaPokemon.splice(this.pokemonSeleccionado, 1);  // Remueve el Pokémon de la lista
+      this.pokemonSeleccionado = null;  // Limpia la selección
     }
   }
 
+  buscarPokemonPorGeneracion(dato: any) {
+    if (dato.target.value !== '') {
+      this.pokeService.getPokemonByGeneration(dato.target.value).subscribe(
+        {
+          next: (respuesta) => {
+            if (respuesta != undefined) {
+              this.listaPokemon = [];  // Limpia la lista antes de cargar nuevos Pokémon
+              respuesta.pokemon_species.map((pokemonEspecie) => {
+                this.pokeService.getPokemonByName(pokemonEspecie.name).subscribe(
+                  {
+                    next: (poke: Pokemon | undefined) => {
+                      if (poke != undefined) {
+                        this.listaPokemon.push(poke);
+                      }
+                    }
+                  }
+                )
+              })
+            }
+          }
+        }
+      )
+    } else {
+      this.listaPokemon = [];
+    }
+  }
 }
-
-
-
