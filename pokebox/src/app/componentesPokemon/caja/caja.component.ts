bef00a356e52480e4c70bd558140948ab773483f
malotes-en-el-techo <lucasivanhourcade@gmail.com>
2024-11-03 13:27:53 -0300

    ahora solo se puede hasta un maximo de 30 pokemon
    y cambios visuales

--- pokebox/src/app/componentesPokemon/caja/caja.component.ts
+++ pokebox/src/app/componentesPokemon/caja/caja.component.ts
@@ -10,9 +10,14 @@ import { Pokemon } from '../../interfazpokemon/interfazpokemon.inteface';
   styleUrls: ['./caja.component.css']
 })
 export class CajaComponent {
+  private readonly MAX_POKEMON = 30; // Límite máximo de Pokémon en la caja
   pokemonesEnCaja: Pokemon[] = []; // Lista de Pokémon en la caja
 
   agregarPokemon(pokemon: Pokemon) {
-    this.pokemonesEnCaja.push(pokemon);
+    if (this.pokemonesEnCaja.length < this.MAX_POKEMON) {
+      this.pokemonesEnCaja.push(pokemon);
+    } else {
+      console.warn('No se pueden agregar más Pokémon a la caja. Límite alcanzado.');
+    }
   }
 }
