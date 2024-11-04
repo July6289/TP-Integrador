4bcb2897ca6468522e2e431e9d93e532caf2ae06
JuliAuteri <auterijuli82@gmail.com>
2024-11-04 11:59:16 -0300

    modificacion de rutas innecesarias y arreglo de errores con los imports

--- pokebox/src/app/app.routes.ts
+++ pokebox/src/app/app.routes.ts
@@ -1,44 +1,35 @@
 import { Routes } from '@angular/router';
-import { PokemonpagesComponent } from './pokemonpages/pokemonpages.component';
 import { PerfilComponent } from './componentesPokemon/perfil/perfil.component';
 import { EquipoPokemonComponent } from './componentesPokemon/equipo-pokemon/equipo-pokemon.component';
 import { PaginaPrincipalComponent } from './componentesPokemon/pagina-principal/pagina-principal.component';
-import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { ListaEquipoPokemonComponent } from './componentesPokemon/lista-equipo-pokemon/lista-equipo-pokemon.component';
 import { PaginaLogueoComponent } from './componentesPokemon/pagina-logueo/pagina-logueo.component';
 
-export const routes: Routes = [
-{
-  path: 'perfil',
-  component: PerfilComponent
-},
-{
-  path: 'equipo-pokemon',
-  component: EquipoPokemonComponent
-},
-{
-  path:'lista-pokemon',
-  component:ListaEquipoPokemonComponent
-},
-{
-  path:'app-pokemonpages',
 
-  component:PokemonpagesComponent,
-},
-{
-  path:'main-page',
-  component: PaginaPrincipalComponent,
-},
-{
-  path:'search-pokemon',
-  component:SearchPokemonComponent,
-},
-{
-  path:'loggin-page',
-  component:PaginaLogueoComponent,
-},
-{
-  path:'**',
-  redirectTo: 'main-page'
-}
+
+export const routes: Routes = [
+  {
+    path: 'perfil',
+    component: PerfilComponent
+  },
+  {
+    path: 'equipo-pokemon',
+    component: EquipoPokemonComponent
+  },
+  {
+    path: 'lista-pokemon',
+    component: ListaEquipoPokemonComponent
+  },
+  {
+    path: 'main-page',
+    component: PaginaPrincipalComponent,
+  },
+  {
+    path: 'loggin-page',
+    component: PaginaLogueoComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'main-page'
+  }
 ];
--- pokebox/src/app/componentesPokemon/pagina-logueo/pagina-logueo.component.ts
+++ pokebox/src/app/componentesPokemon/pagina-logueo/pagina-logueo.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagina-logueo',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './pagina-logueo.component.html',
   styleUrl: './pagina-logueo.component.css'
 })
 export class PaginaLogueoComponent {
+
+
+  fb=inject(FormBuilder)
+
+  formulario=this.fb.nonNullable.group(
+    {
+
+
+
+    }
+  )
+
 
 }
