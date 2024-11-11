f63c58b897f4e25138237ad1f12346e727a3706d
Lucas Hourcade <lucasivanhourcade@gmail.com>
2024-11-11 12:31:04 -0300

    crear tabla de tipos
    ahora a ver si funciona

--- pokebox/src/app/componentesPokemon/pestania-combate/pestania-combate.component.ts
+++ pokebox/src/app/componentesPokemon/pestania-combate/pestania-combate.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ListaEquipoPokemonComponent } from "../lista-equipo-pokemon/lista-equipo-pokemon.component";
+import { Pokemon } from '../../interfaces/interfazpokemon/interfazpokemon.inteface';
+import { isNull } from 'lodash';
 
 @Component({
   selector: 'app-pestania-combate',
@@ -9,5 +11,93 @@ import { ListaEquipoPokemonComponent } from "../lista-equipo-pokemon/lista-equip
   styleUrl: './pestania-combate.component.css'
 })
 export class PestaniaCombateComponent {
+  private tablaTiposValores: number[][] = [
+    [0.5, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 1], //normal
+    [2, 1, 0.5, 1, 1, 0, 1, 0.5, 2, 1, 2, 1, 0.5, 2, 2, 1, 0.5, 0.5], //lucha
+    [0.5, 1, 2, 1, 0.5, 1, 1, 1, 1, 2, 1, 2, 1, 0.5, 1, 1, 1, 1], //volador
+    [0, 1, 1, 1, 1, 0.5, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 1], //veneno
+    [2, 1, 0.5, 1, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 2, 1, 1, 2, 0], //tierra
+    [0.5, 1, 2, 1, 1, 1, 2, 1, 2, 0.5, 1, 1, 1, 1, 1, 0.5, 1, 2], //roca
+    [0.5, 1, 1, 1, 1, 0.5, 0.5, 0.5, 1, 0.5, 1, 2, 2, 1, 2, 1, 0.5, 0.5], //bicho
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 1, 2, 1, 0.5, 1, 1, 1], //fantasma
+    [0.5, 0.5, 1, 1, 0.5, 1, 0.5, 2, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1], //acero
+    [2, 0.5, 2, 0.5, 1, 1, 0.5, 1, 2, 1, 1, 2, 1, 0.5, 1, 1, 1, 1],  //fuego
+    [1, 0.5, 1, 0.5, 1, 1, 2, 1, 1, 1, 1, 0.5, 1, 2, 1, 2, 1, 1], //agua
+    [0.5, 2, 0.5, 0.5, 1, 1, 0.5, 1, 1, 1, 1, 0.5, 1, 2, 1, 2, 0.5, 0.5], //planta
+    [1, 2, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 0, 1, 2], //electrico
+    [0.5, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 1, 0, 1, 2, 1], //psiquico
+    [0.5, 0.5, 1, 2, 1, 1, 0.5, 1, 0.5, 1, 1, 2, 1, 1, 1, 2, 1, 2], //hielo
+    [0.5, 1, 1, 2, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], //dragon
+    [1, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 1, 1, 2, 1, 0.5, 1, 1, 1], //siniestro
+    [0.5, 1, 1, 2, 1, 1, 0.5, 1, 1, 2, 1, 1, 1, 1, 2, 1, 0.5, 1], //hada
+  ]
+
+  private tablaTiposNombres: string[] = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"]
+
+
+  tablaDeTipos(pokemonJugador: Pokemon, pokemonBot: Pokemon, turno: boolean) {
+    let danio = 0;
+    let indiceTipos = {
+      jugadorTipo1: -1,
+      jugadorTipo2: -1,
+      botTipo1: -1,
+      botTipo2: -1
+    }
+
+    //buscar indice tipos tipos
+    for (let i = 0; i < this.tablaTiposNombres.length; i++) {
+      if (pokemonJugador.types[0].type.name === this.tablaTiposNombres[i]) {
+        indiceTipos.jugadorTipo1 = i;
+      }
+
+      if (pokemonJugador.types[1] && pokemonJugador.types[1].type.name === this.tablaTiposNombres[i]) {
+        indiceTipos.jugadorTipo2 = i;
+      }
+
+      if (pokemonBot.types[0].type.name === this.tablaTiposNombres[i]) {
+        indiceTipos.botTipo1 = i;
+      }
+
+      if (pokemonBot.types[1] && pokemonBot.types[1].type.name === this.tablaTiposNombres[i]) {
+        indiceTipos.botTipo2 = i;
+      }
+    }
+    /*---------------------*/
+
+    //calculadora de daño
+    if (turno) {
+
+      //los pokemon van a tener 64 de vida
+
+      if (indiceTipos.jugadorTipo2 !== -1 && indiceTipos.botTipo2 !== -1) {
+        danio = 4 * ((this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo1] * this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo2]) * (this.tablaTiposValores[indiceTipos.jugadorTipo2][indiceTipos.botTipo1] * this.tablaTiposValores[indiceTipos.jugadorTipo2][indiceTipos.botTipo2]));
+      }
+      else if (indiceTipos.jugadorTipo2 !== -1 && indiceTipos.botTipo2 === -1) {
+        danio = 4 * (this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo1] * this.tablaTiposValores[indiceTipos.jugadorTipo2][indiceTipos.botTipo1]);
+      }
+      else if (indiceTipos.jugadorTipo2 === -1 && indiceTipos.botTipo2 !== -1) {
+        danio = 4 * (this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo1] * this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo2]);
+      }
+      else {
+        danio = 4 * this.tablaTiposValores[indiceTipos.jugadorTipo1][indiceTipos.botTipo1];
+      }
+    }
+    else {
+      if (indiceTipos.botTipo2 !== -1 && indiceTipos.botTipo2 !== -1) {
+        danio = 4 * ((this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo1] * this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo2]) * (this.tablaTiposValores[indiceTipos.botTipo2][indiceTipos.jugadorTipo1] * this.tablaTiposValores[indiceTipos.botTipo2][indiceTipos.jugadorTipo2]));
+      }
+      else if (indiceTipos.botTipo2 !== -1 && indiceTipos.botTipo2 === -1) {
+        danio = 4 * (this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo1] * this.tablaTiposValores[indiceTipos.botTipo2][indiceTipos.jugadorTipo1]);
+      }
+      else if (indiceTipos.botTipo2 === -1 && indiceTipos.botTipo2 !== -1) {
+        danio = 4 * (this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo1] * this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo2]);
+      }
+      else {
+        danio = 4 * this.tablaTiposValores[indiceTipos.botTipo1][indiceTipos.jugadorTipo1];
+      }
+    }
+
+    console.log(danio);
+  }
 
 }
