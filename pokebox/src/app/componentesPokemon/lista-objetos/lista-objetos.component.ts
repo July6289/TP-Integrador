de7e1771ba604186d10354caecbcc3a71f1e46da
malotes-en-el-techo <lucasivanhourcade@gmail.com>
2025-05-02 16:47:50 -0300

    componentes linkeados
    necesitaremos un service?

--- pokebox/src/app/componentesPokemon/lista-objetos/lista-objetos.component.ts
+++ pokebox/src/app/componentesPokemon/lista-objetos/lista-objetos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InfoObjetoComponent } from "./info-objeto/info-objeto.component";
+import { InventarioObjetoComponent } from "./inventario-objeto/inventario-objeto.component";
+import { BuscadorObjetoComponent } from "./buscador-objeto/buscador-objeto.component";
 
 @Component({
   selector: 'app-lista-objetos',
   standalone: true,
-  imports: [],
+  imports: [InfoObjetoComponent, InventarioObjetoComponent, BuscadorObjetoComponent],
   templateUrl: './lista-objetos.component.html',
   styleUrl: './lista-objetos.component.css'
 })
